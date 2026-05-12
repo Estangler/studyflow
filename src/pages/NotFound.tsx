ce2be8a7a33d0c5a98912adb462fd5a9ac507af2
Estangler <estanglerv@gmail.com>
2026-05-11 23:22:24 -0300

    feat: Criado o componente de registro, adicionado link para navegação da pagina login na pagina NotFound e criado arquivos AuthContext, useAuth. fix: corrigido typo na importação do AppRouter estava appRouter.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h1>NotFound</h1>
+      <Link to={"/"}>Login</Link>
+    </div>
+  );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,52 @@
+import { Link } from "react-router-dom";
+
 export default function Register() {
-  return <div>Hello Register!</div>;
+  return (
+    <div>
+      <header>
+        <h1>Hello Register!</h1>
+        <Link to={"/"}>Faça login.</Link>
+      </header>
+      <main>
+        <form>
+          <label>
+            <p>
+              Nome completo <span className="text-red-600">*</span>
+            </p>
+            <input
+              type="text"
+              placeholder="Ex: João da Silva Sauro"
+              className="border px-2"
+            />
+          </label>
+
+          <label>
+            <p>
+              E-mail <span className="text-red-600">*</span>
+            </p>
+            <input
+              type="email"
+              placeholder="Ex: joao@email.com"
+              className="border px-2"
+            />
+          </label>
+
+          <label>
+            <p>
+              Senha <span className="text-red-600">*</span>
+            </p>
+            <input
+              type="password"
+              placeholder="********"
+              className="border px-2"
+            />
+            <p>Mínimo 8 caracteres.</p>
+          </label>
+
+          <button className="w-50 h-10 px-2 py-1 border">Criar conta</button>
+        </form>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
