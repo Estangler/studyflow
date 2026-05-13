a34cc5c09259e1f35251780bf6307c5d3b036c8e
Estangler <estanglerv@gmail.com>
2026-05-13 15:47:04 -0300

    feat: refactor Dashboard component to display Kanban columns

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,16 @@
-import { useAuth } from "../hooks/useAuth";
+import {
+  COLUMNS,
+  KANBAN_COLUMNS,
+} from "../features/kanban/constants/constants";
 
 export default function Dashboard() {
-  const { currentUser, logout } = useAuth();
   return (
-    <div>
-      Hello {currentUser?.name}
-      <button onClick={logout}>Sair.</button>
+    <div className="px-4 py-2">
+      {COLUMNS.map((col) => (
+        <div>
+          <p>{KANBAN_COLUMNS[col]}</p>
+        </div>
+      ))}
     </div>
   );
 }
